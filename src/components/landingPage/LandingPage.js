1235422dabba6b54b2645e4bbe39289432d81d62
Owen Dods <owendods@gmail.coom>
2020-08-22 16:45:37 +0100

    First pass at styling the LandingPage

--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export const className = 'landingPage';
 
-const LandingPage = () => (<div className={className} />);
+const LandingPage = () => (
+
+	<div className={className}>
+
+		<h1>Real Achievements</h1>
+
+	</div>
+
+);
 
 export default LandingPage;
