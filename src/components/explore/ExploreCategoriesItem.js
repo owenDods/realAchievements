87596866a68685858c7f8a73146689aafc6a9dd0
Owen Dods <owendods@gmail.com>
2020-11-08 13:47:41 +0000

    Add in iconography to ExploreCategoriesItem and extend them to be Links

--- src/components/explore/ExploreCategoriesItem.js
+++ src/components/explore/ExploreCategoriesItem.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
+
+import get from 'lodash/fp/get';
+
+import categoryIconMap from './utils/categoryIconMap';
+
+import Button from '../button/Button';
 
 export const className = 'exploreCategoriesItem';
 
-const ExploreCategoriesItem = ({ name }) => (
+const ExploreCategoriesItem = ({ name, icon }) => (
 
 	<div className={className}>
 
-		<p className={`${className}__name`}>{name}</p>
+		<Button
+			label={name}
+			icon={get(icon, categoryIconMap)}
+			textPosition="bottom"
+			isLink
+			to={`${useLocation().pathname}/${name}`}
+		/>
 
 	</div>
 
 );
 
 ExploreCategoriesItem.propTypes = {
-	name: PropTypes.string
+	name: PropTypes.string,
+	icon: PropTypes.string
 };
 
 export default ExploreCategoriesItem;
---
+++ src/components/explore/utils/categoryIconMap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Nature from '../../../img/nature.svg';
+import Sport from '../../../img/sport.svg';
+import Meal from '../../../img/meal.svg';
+import Culture from '../../../img/culture.svg';
+import Gaming from '../../../img/gaming.svg';
+import Milestone from '../../../img/milestone.svg';
+
+export default {
+	nature: (<Nature />),
+	sport: (<Sport />),
+	meal: (<Meal />),
+	culture: (<Culture />),
+	gaming: (<Gaming />),
+	milestone: (<Milestone />)
+};
